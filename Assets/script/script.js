32aff11071494b60cc01a2de878cfc23ad542855
ankit-jds <brilliankitamboli@gmail.com>
2022-04-18 19:18:57 +0530

    Cart Page and cart js done

--- Assets/script/script.js
+++ Assets/script/script.js
@@ -39,38 +39,43 @@ window.onload = () => {
     console.log("window is loaded....");
 
     let accordian_content_array = document.getElementsByClassName("accordian_content");
-    for (let index = 0; index < accordian_content_array.length; index++) {
-        const accordian_content = accordian_content_array[index];
-        // console.log(accordian_content, "ac");
+    if (accordian_content_array) {
+        for (let index = 0; index < accordian_content_array.length; index++) {
+            const accordian_content = accordian_content_array[index];
+            // console.log(accordian_content, "ac");
 
-        let name = accordian_content.id;
-        // console.log(name, "name ac");
+            let name = accordian_content.id;
+            // console.log(name, "name ac");
 
-        let data_obj = data[name.split('_')[0]];
-        // console.log(data_obj);
+            let data_obj = data[name.split('_')[0]];
+            // console.log(data_obj);
 
-        let data_obj_keys = Object.keys(data_obj);
-        // console.log(data_obj_keys);
+            let data_obj_keys = Object.keys(data_obj);
+            // console.log(data_obj_keys);
 
-        let html_to_be_added = '';
+            let html_to_be_added = '';
 
-        data_obj_keys.forEach((key) => {
-            // console.log(key);
-            const code = `
-                <label for="${key}">
-                    <input type="checkbox" name="${name}" id="${key}" data-price="${data_obj[key]}">
-                    ${key} : ${data_obj[key]}
-                </label>`
-            // console.log(code);
-            html_to_be_added += code;
-        })
-        accordian_content.innerHTML = html_to_be_added;
+            data_obj_keys.forEach((key) => {
+                // console.log(key);
+                const code = `
+                    <label for="${key}">
+                        <input type="checkbox" name="${name}" id="${key}" data-price="${data_obj[key]}">
+                        ${key} : ${data_obj[key]}
+                    </label>`
+                // console.log(code);
+                html_to_be_added += code;
+            })
+            accordian_content.innerHTML = html_to_be_added;
 
+        }
     }
 
     let site_wrapper = document.getElementById("site_wrapper");
     if (site_wrapper) {
         console.log("Site wrapper exists....");
+
+        localStorage.removeItem('Orders');
+
         site_wrapper.addEventListener('click', (e) => {
             console.log("Site Wrapper is clicked...");
             let target = e.target;
@@ -94,7 +99,7 @@ window.onload = () => {
                 // console.log(menu);
             }
             if (target.tagName === "INPUT") {
-                
+
                 console.log("input i s pressed...");
                 let dishname = target.id;
                 let price = target.getAttribute("data-price");
@@ -112,13 +117,13 @@ window.onload = () => {
                     }
                     console.log(Order_list);
                 }
-                if(target.getAttribute('name')==="quantity"){
+                if (target.getAttribute('name') === "quantity") {
                     console.log("Quantity is changed...");
-                    let parent=target.parentElement;
-                    let sibling=parent.previousElementSibling;
-                    console.log(parent,sibling.textContent);
-                    let ordered_dishname=sibling.textContent;
-                    Order_list[ordered_dishname].quantity=target.value;
+                    let parent = target.parentElement;
+                    let sibling = parent.previousElementSibling;
+                    console.log(parent, sibling.textContent);
+                    let ordered_dishname = sibling.textContent;
+                    Order_list[ordered_dishname].quantity = target.value;
                     console.log(Order_list);
                 }
             }
@@ -133,7 +138,7 @@ window.onload = () => {
                         <div>
                             <p>${key}</p>
                             <label for="${key + "_quantity"}">
-                                <input type="number" name="quantity" id="${key + "_quantity"}" value="${Order_list[key].quantity}" min="1">
+                                <input class="outline outline-1 " type="number" name="quantity" id="${key + "_quantity"}" value="${Order_list[key].quantity}" min="1">
                             </label>
                             <p>${Order_list[key].price}</p>
                         </div>
@@ -144,8 +149,41 @@ window.onload = () => {
                     order_cart.innerHTML = html_to_be_added;
                 }
             }
-        })
+
+            if (target.id === "place_order" || target.id === "place_order_text") {
+                console.log("Order is placed...");
+                console.log(Order_list);
+                localStorage.setItem("Orders", JSON.stringify(Order_list));
+                location.href='./Assets/pages/cart.html';
+            }
+        }, false)
     }
 
+    let cart_table_body=document.getElementById('cart_table_body');
+    if(cart_table_body){
+        let Order_list=JSON.parse(localStorage.getItem('Orders'));
+        console.log(Order_list);
+        let html_to_be_added=``;
+
+        let Order_list_keys=Object.keys(Order_list);
+        Order_list_keys.forEach((key)=>{
+            let dish=Order_list[key];
+
+            let code=`
+            <tr>
+                <td>${key}</td>
+                <td>${dish.quantity}</td>
+                <td>${dish.price}</td>
+                <td>${dish.price * dish.quantity}</td>
+            </tr>
+            `
+
+            html_to_be_added+=code;
+        });
+
+        cart_table_body.innerHTML+=html_to_be_added;
+        console.log();
+
+    }
 
 }
